23a7ee68276dfe7d0af68e8ed65e96b784b7bc68
Marvin <jmarvin.ramoda@gmail.com>
2026-02-16 19:09:28 +0800

    message sending to llm already works

--- app/services/Message/ai_message.py
+++ app/services/Message/ai_message.py
@@ -2,14 +2,10 @@ from app.services.LLM import OllamaModel
 from app.services.Prompt import PromptTemplate
 from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
-        # llm = LLMModel().get()
-        # prompt = Prompt().get()
-        # output_parser = StrOutputParser()   
+
 class AIMessage:
     def __init__(self):
         pass
-
-
 
     def get(self, human_message):
         llm = OllamaModel().init()
